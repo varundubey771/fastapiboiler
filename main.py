d4f554c0823557c18d0ae2d326e676857d31319d
varundubey771 <varundubey771@gmail.com>
2024-03-27 23:15:32 +0530

    flask

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI
+from flask import Flask
+app = Flask(__name__)
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
